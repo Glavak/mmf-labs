353442b225294179d0566b82a4d0dd3d578cf513
GLAVAK <gau-gau@ya.ru>
2017-09-12 07:42:37 +0700

    Задания на четвёрку с тестовым main'ом и заметками

--- list/main.cpp
+++ list/main.cpp
@@ -11,7 +11,23 @@ int main()
         primes.push(3);
         primes.push(5);
 
-        std::cout << "Expected '2 3 5': " << primes.pop() << " " << primes.pop() << " " << primes.pop() << std::endl;
+        std::cout << "Expected '2 3 5': "
+                  << primes.pop() << " " << primes.pop() << " " << primes.pop() << std::endl;
+    }
+
+    // На четвёрку:
+    {
+        mylist<int> odds;
+
+        odds.push_back(3);
+        odds.push_back(5);
+        odds.push_back(7);
+
+        std::cout << "Expected '3 5 7': "
+                  << odds.pop_back() << " " << odds.pop_back() << " " << odds.pop_back() << std::endl;
+
+        mylist<int> other;
+        // odds = other;  <--- это не скомпилируется, так как запрещён оператор присваивания
     }
 
     return 0;
--- list/mylist.h
+++ list/mylist.h
@@ -5,6 +5,8 @@
 #ifndef LIST_MYLIST_H
 #define LIST_MYLIST_H
 
+#include <usp10.h>
+
 template<class TElement>
 class mylist
 {
@@ -13,9 +15,19 @@ public:
 
     ~mylist();
 
-    void push(TElement value);
+    // Запрет использования конструктора копирования
+    mylist(const mylist & other) = delete;
+
+    // Запрет использования оператора присваивания
+    mylist & operator=(const mylist & other) = delete;
+
+    void push(const TElement & value);
 
     TElement pop();
+
+    void push_back(const TElement & value);
+
+    TElement pop_back();
 
 private:
     // Нам понадобится ещё одна структура/класс, чтобы хранить цепочку элементов списка
@@ -52,7 +64,7 @@ mylist<TElement>::~mylist()
 
 // Метод вставки элемента в начало
 template<class TElement>
-void mylist<TElement>::push(TElement value)
+void mylist<TElement>::push(const TElement & value)
 {
     listItem<TElement> * item = new listItem<TElement>();
     item->value = value;
@@ -75,6 +87,58 @@ TElement mylist<TElement>::pop()
     listItem<TElement> * newHead = head->next;
     delete head;
     head = newHead;
+
+    return value;
+}
+
+// Метод вставки элемента в конец
+template<class TElement>
+void mylist<TElement>::push_back(const TElement & value)
+{
+    listItem<TElement> * newItem = new listItem<TElement>();
+    newItem->value = value;
+    newItem->next = nullptr;
+
+    if (head == nullptr)
+    {
+        head = newItem;
+    }
+    else
+    {
+        listItem<TElement> * current = head;
+        while (current->next != nullptr)
+        {
+            current = current->next;
+        }
+        current->next = newItem;
+    }
+}
+
+// Метод удаления (и получения) элемента с конца
+template<class TElement>
+TElement mylist<TElement>::pop_back()
+{
+    if (head == nullptr)
+    {
+        // Бросаем ошибку при пустом списке
+        throw;
+    }
+
+    if (head->next == nullptr)
+    {
+        // Если один элемент - то тоже самое что и удалить из начала
+        return pop();
+    }
+
+    listItem<TElement> * current = head;
+    while (current->next->next != nullptr)
+    {
+        current = current->next;
+    }
+
+    TElement value = current->next->value;
+    delete current->next;
+    current->next = nullptr;
 
     return value;
 }
